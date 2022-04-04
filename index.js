0d8fd1c869a11a45a9dc534d9da8def7609558f2
Justin Li <justinlit23@gmail.com>
2022-04-04 14:22:44 -0700

    commit tree dc347218ea1d28306d22683113b2ddc619b7b14a

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(travelDistance){
+  if (travelDistance <= 400) {
+    return 'This one is on me!';
+} else if (travelDistance > 400 && travelDistance < 2000) {
+    return 'That will be twenty bucks.';
+} else if (travelDistance > 2000 && travelDistance < 2500) {
+    return 'I will gladly take your thirty bucks.';
+} else if (travelDistance > 2500) {
+    return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city) {
+  if (city == 'NYC') {
+    return 'Ok, sounds good.'
+  } else if (city !== 'NYC') {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+/*
+function ternaryCheckCity(city) {
+  city == 'NYC' ? 'Ok, sounds good.' : "No go.";
+}*/
+
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.';
+    break;
+  default:
+    return 'Bye.';
+    break;
+  }
 }
